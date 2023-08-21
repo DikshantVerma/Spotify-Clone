53a5d1a826aabab52baf504811caeaef8153bcae
DikshantVerma <dikshantverma411@gmail.com>
2023-08-21 23:29:52 +0530

    Added Spotify logo

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Login.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      Login page
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default Login;
