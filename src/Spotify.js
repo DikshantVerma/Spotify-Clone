f29ac785550c36d01a328ea8cf977fe62d3bfaa2
DikshantVerma <dikshantverma411@gmail.com>
2023-08-22 00:17:51 +0530

    Added scopes

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,3 +1,15 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
+
+const clientId = "f88d15b1a8ef447ea0d9c4484c07a73a";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-currently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?`;
