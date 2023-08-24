39212f0b2581d3e6091ac06ceb5c1b8f89f5047d
DikshantVerma <dikshantverma411@gmail.com>
2023-08-25 00:12:43 +0530

    Deleted unwanted files plus Spotify token working

--- src/spotifyaccess.js
+++ src/spotifyaccess.js
@@ -6,7 +6,6 @@ const clientId = "f88d15b1a8ef447ea0d9c4484c07a73a";
 
 const scopes = [
   "user-read-currently-playing",
-  "user-read-currently-played",
   "user-read-playback-state",
   "user-top-read",
   "user-modify-playback-state",
