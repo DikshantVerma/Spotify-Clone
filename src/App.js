cebfbb52e6ff40dc86df2f452979e18cab242412
DikshantVerma <dikshantverma411@gmail.com>
2023-08-25 00:56:02 +0530

    Token access

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotifyaccess";
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log("I have a token", token);
+  }, []);
+
   return (
-    <div className="App">
+    <div className="app">
       <Login />
     </div>
   );
