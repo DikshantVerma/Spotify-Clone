152bc86cbb420ef33eba6f3dc7a47a2c2670e0d5
DikshantVerma <dikshantverma411@gmail.com>
2023-08-25 01:03:38 +0530

    Properly logged in

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotifyaccess";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
     console.log("I have a token", token);
   }, []);
 
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
