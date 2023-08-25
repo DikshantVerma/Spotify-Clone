3a50d2854c898144c26413ba979a36e75d304b2e
DikshantVerma <dikshantverma411@gmail.com>
2023-08-26 00:06:54 +0530

    Used token to login once

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import { useDataLayerValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-
-  const [{user}, dispatch] = useDataLayerValue();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -20,7 +18,10 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  token:
+    "BQCeq0wtMePx2f_SbmkCPPJu_KXgKScomNaE3N_O4emsxS-c1jLZ60GU0YwCWJQVSTj9RT4fjxkcNeQYFD-xm-0rQb8EKf6w6zn6j6Nz0EBIm2zq7fTk_P6-AYfdU1QFYOMpddBCDUcM6F0b7j8068zynyHYG9ieqiAgGZR0kIHvbRUMZu1CgA_srvkqv5n-YS49l2kj75sdRpEfXOAWng",
 };
 
 const reducer = (state, action) => {
@@ -14,6 +16,13 @@ const reducer = (state, action) => {
         ...state,
         user: action.user,
       };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        user: action.token,
+      };
+
     default:
       return state;
   }
