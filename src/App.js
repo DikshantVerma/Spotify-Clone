1836b019709a06822b4e42a2480fbe015b0605a5
DikshantVerma <dikshantverma411@gmail.com>
2023-08-25 23:05:58 +0530

    Installed spotify web-api-js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotifyaccess";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -14,6 +17,7 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
     }
 
     console.log("I have a token", token);
