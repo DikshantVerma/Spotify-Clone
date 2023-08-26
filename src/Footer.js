08264c303e1f37ed79bc7163ecc30a0576ff26a5
DikshantVerma <dikshantverma411@gmail.com>
2023-08-26 15:38:46 +0530

    Added all the playback icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+
+      <div className="footer__centre">
+        <p>Player controls</p>
+      </div>
+
+      <div className="footer__right">
+        <p>Volume control</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
